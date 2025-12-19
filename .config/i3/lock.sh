75f48d9e507c6a7ea811cc95fae4fd6d63763035
@apecout_dcms <antoine.pecout@dcms-solutions.com>
2025-12-19 13:41:57 +0100

    gruv-box ulauncher theme

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-alpha='dd'
-background='#282a36'
-selection='#44475a'
+alpha='ff'
+background='#282828'
+selection='#282828'
 comment='#6272a4'
 
-yellow='#f1fa8c'
-orange='#ffb86c'
-red='#ff5555'
-magenta='#ff79c6'
-blue='#6272a4'
-cyan='#8be9fd'
-green='50fa7b'
+yellow='#fabd2f'
+orange='#d79921'
+red='#cc241d'
+magenta='#b16286'
+blue='#458588'
+cyan='#689d6a'
+green='#98971a'
 
 i3lock \
   --insidever-color=$selection$alpha \
@@ -21,35 +21,35 @@ i3lock \
   --ringwrong-color=$red$alpha \
   --ringver-color=$green$alpha \
   --ringwrong-color=$red$alpha \
-  --ring-color=$blue$alpha \
+  --ring-color=$magenta$alpha \
   --line-uses-ring \
-  --keyhl-color=$magenta$alpha \
+  --keyhl-color=$orange$alpha \
   --bshl-color=$orange$alpha \
   --separator-color=$selection$alpha \
   --verif-color=$green \
   --wrong-color=$red \
   --modif-color=$red \
   --layout-color=$blue \
-  --date-color=$blue \
-  --time-color=$blue \
+  --date-color=$yellow \
+  --time-color=$yellow \
   --screen 1 \
   --blur 1 \
   --clock \
   --indicator \
   --time-str="%H:%M:%S" \
-  --date-str="%A %e %B %Y" \
+  --date-str="%a %e %b %Y" \
   --verif-text="Checking..." \
   --wrong-text="Wrong pswd" \
   --noinput="No Input" \
   --lock-text="Locking..." \
   --lockfailed="Lock Failed" \
   --radius=120 \
-  --ring-width=10 \
+  --ring-width=15 \
   --pass-media-keys \
   --pass-screen-keys \
   --pass-volume-keys \
-#  --time-font="JetBrainsMono Nerd Font" \
-#  --date-font="JetBrainsMono Nerd Font" \
-#  --layout-font="JetBrainsMono Nerd Font" \
-#  --verif-font="JetBrainsMono Nerd Font" \
-#  --wrong-font="JetBrainsMono Nerd Font" \
+  --time-font="DepartureMono Nerd Font Mono" \
+  --date-font="DepartureMono Nerd Font Mono" \
+  --verif-font="DepartureMono Nerd Font Mono" \
+  --wrong-font="DepartureMono Nerd Font Mono" \
+  --layout-font="DepartureMono Nerd Font Mono" \
