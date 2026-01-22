f706405a74baa2a055b2652af6cd0df1735ba1cd
Antoine Pécout <antoine.pecout@dcms-solutions.com>
2026-01-22 14:20:22 +0100

    update de jnspq

---
+++ .config/nvim/lua/plugins/gruvbox-material.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_transparent_background = 1
+    end,
+  },
+}
